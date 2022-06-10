2c3145c840a7f0fe53bd5fcb425bf725656718fe
Lauren Porch <loporch@gmail.com>
2022-06-10 08:52:38 -0600

    cleanup pie chart example

--- src/App.js
+++ src/App.js
@@ -5,21 +5,7 @@ import GanttChart from './GanttChart';
 function App() {
   return (
     <div className="App">
-        <GanttChart />
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+        <GanttChart paddingRight={20}/>
     </div>
   );
 }
--- src/GanttChart.js
+++ src/GanttChart.js
@@ -3,13 +3,10 @@ import * as am5 from '@amcharts/amcharts5';
 import * as am5percent from '@amcharts/amcharts5/percent';
 
 class GanttChart extends React.Component {
+    // All chart creation should take place here
     componentDidMount() {
-        // am5.ready(() => {
-            const root = am5.Root.new("chartdiv");
-        // });
-
+        const root = am5.Root.new("chartdiv");
         const chart = root.container.children.push(am5percent.PieChart.new(root, {}));
-
         const series = chart.series.push(
             am5percent.PieSeries.new(
                 root, {
@@ -19,6 +16,7 @@ class GanttChart extends React.Component {
             )
         );
 
+        // Data - add this last!
         series.data.setAll([{
             category: "Research",
             value: 1000
@@ -30,18 +28,28 @@ class GanttChart extends React.Component {
             value: 850
         }]);
 
+        // Set objects
+        this.chart = chart;
         this.root = root;
     }
 
     render() {
         return(
             <>
-            Made a thing.
+            {/* Render the chart itself inside a div. id should match whatever we passed to Root.new() */}
             <div id="chartdiv" style={{width: "100%", height: "500px"}}></div>
             </>
         );
     }
 
+    // All chart updates need to be checked and updated here
+    componentDidUpdate() {
+        if (oldProps.paddingRight !== this.props.paddingRight) {
+            this.chart.set("paddingRight", this.props.paddingRight);
+        }
+    }
+
+    // Make sure to dispose of chart when we're done!
     componentWillUnmount() {
         if (this.root) {
             this.root.dispose();
